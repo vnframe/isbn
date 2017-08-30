3dc0c081964dec090ae2f531d6d6f4d9f7d3dca6
vnframe <vnframe95@icloud.com>
2017-08-30 12:37:39 -0400

    ohhhhhh test passes

--- isbnfunct.rb
+++ isbnfunct.rb
@@ -1,7 +1,8 @@
 def num_to_array(id)
     id = id.delete"-"
+    id = id.delete" "
     id = id.chars
-    p id
+     id
 end
 def isbn_ten_length(id)
     num_to_array(id)
@@ -10,4 +11,24 @@ def isbn_ten_length(id)
     else
         false
 end
+end
+def check(id)
+    id = num_to_array(id)
+    #id = id[0..9]
+    sum=0
+
+    id.each_with_index do |value, index|
+        sum = sum + ((index + 1).to_i * value.to_i)
+    end
+    
+    check_digit = sum % 11
+    if check_digit == 10
+        id << "x"
+        digit = "x"
+    elsif check_digit == 0
+        id << "0"
+        digit = 0
+    else isbn << check_digit.to_s
+    end 
+    digit
 end
